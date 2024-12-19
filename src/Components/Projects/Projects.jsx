d45857829f99a604958f6a41c52a7eca7d0ab432
Sammy <96604660+itsmasso@users.noreply.github.com>
2024-12-18 23:05:55 -0800

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import "./Projects.css";
-import project_data from "../../assets/project_data";
+
 
 import tv_scan_lines_2 from "../../assets/tv_scan_lines_2.gif";
-
+import video_game_icon from './invader.svg'
+import food_icon from './food_icon.svg'
+import compiler_icon from './compiler_icon.svg'
 
 const Projects = () => {
+const project_data = [
+    {
+        name:"Pro Angler",
+        image: video_game_icon,
+        description: "A 2D pixel art fishing game made in Unity. Want to see more?",
+        technologies:"Made in Unity's 2D engine and C#.",
+        link: "https://itsmasso.itch.io/pro-angler"
+    },
+    {
+        name:"Recipe Management API",
+        image: food_icon,
+        description: "A CRUD API for recipes that uses token-based authentication. Want to see more?",
+        technologies:"Made using Python, Flask, Redis, and MySQL.",
+        link: "https://github.com/mkt989/CPSC-449-Final-Project"
+    },
+    {
+        name:"Compiler Project",
+        image: compiler_icon,
+        description: "Part of a functional compiler that can parse tokens and convert code to assembly code. Want to see more?",
+        technologies:"Made in Java only!",
+        link: "https://github.com/itsmasso/CompilerProject/tree/main"
+    },
 
+
+]
   return (
     <div id="projects" className="projects">
       <div className="projects-title ">
